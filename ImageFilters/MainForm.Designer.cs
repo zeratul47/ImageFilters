e3d92a7ca19182b905da320b2d88c069a8734fa5
Alexander Gutnikov <zeratul477@gmail.com>
2022-06-05 17:44:47 +0200

    Добавьте файлы проекта.

---
+++ ImageFilters/MainForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace ImageFilters
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.selctImageSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.originalPictureBox = new System.Windows.Forms.PictureBox();
+            this.filteredPictureBox = new System.Windows.Forms.PictureBox();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.selctImageSplitContainer)).BeginInit();
+            this.selctImageSplitContainer.Panel1.SuspendLayout();
+            this.selctImageSplitContainer.Panel2.SuspendLayout();
+            this.selctImageSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filteredPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.filtersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.invertToolStripMenuItem});
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // invertToolStripMenuItem
+            // 
+            this.invertToolStripMenuItem.Name = "invertToolStripMenuItem";
+            this.invertToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.invertToolStripMenuItem.Text = "Invert";
+            this.invertToolStripMenuItem.Click += new System.EventHandler(this.invertToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.selctImageSplitContainer);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.mainPictureBox);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 193;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // selctImageSplitContainer
+            // 
+            this.selctImageSplitContainer.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.selctImageSplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.selctImageSplitContainer.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.selctImageSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selctImageSplitContainer.IsSplitterFixed = true;
+            this.selctImageSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.selctImageSplitContainer.Name = "selctImageSplitContainer";
+            this.selctImageSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // selctImageSplitContainer.Panel1
+            // 
+            this.selctImageSplitContainer.Panel1.Controls.Add(this.originalPictureBox);
+            // 
+            // selctImageSplitContainer.Panel2
+            // 
+            this.selctImageSplitContainer.Panel2.Controls.Add(this.filteredPictureBox);
+            this.selctImageSplitContainer.Size = new System.Drawing.Size(193, 426);
+            this.selctImageSplitContainer.SplitterDistance = 180;
+            this.selctImageSplitContainer.TabIndex = 0;
+            // 
+            // originalPictureBox
+            // 
+            this.originalPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.originalPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.originalPictureBox.Name = "originalPictureBox";
+            this.originalPictureBox.Size = new System.Drawing.Size(191, 178);
+            this.originalPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.originalPictureBox.TabIndex = 0;
+            this.originalPictureBox.TabStop = false;
+            this.originalPictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // filteredPictureBox
+            // 
+            this.filteredPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filteredPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.filteredPictureBox.Name = "filteredPictureBox";
+            this.filteredPictureBox.Size = new System.Drawing.Size(191, 240);
+            this.filteredPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.filteredPictureBox.TabIndex = 0;
+            this.filteredPictureBox.TabStop = false;
+            this.filteredPictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(601, 424);
+            this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Image filters";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.selctImageSplitContainer.Panel1.ResumeLayout(false);
+            this.selctImageSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.selctImageSplitContainer)).EndInit();
+            this.selctImageSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filteredPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invertToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.SplitContainer selctImageSplitContainer;
+        private System.Windows.Forms.PictureBox originalPictureBox;
+        private System.Windows.Forms.PictureBox filteredPictureBox;
+    }
+}
+
---
+++ ImageFilters/MainForm.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageFilters
+{
+    public partial class MainForm : Form
+    {
+        #region FIELDS
+
+        /// <summary>
+        /// File dialog to open image.
+        /// </summary>
+        OpenFileDialog _openFileDialog;
+
+        /// <summary>
+        /// Original image.
+        /// </summary>
+        Bitmap _originalImage;
+
+        /// <summary>
+        /// Original image after filtering.
+        /// </summary>
+        Bitmap _filteredImage;
+
+        #endregion
+
+
+        #region METHODS
+
+        #region PUBLIC
+
+        /// <summary>
+        /// Initialization of the viewer.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _openFileDialog = new OpenFileDialog();
+            _openFileDialog.Filter = 
+                "bitmap files (*.bmp)|*.bmp|" +
+                "jpeg files (*.jpg; *.jpeg)|*.jpg; *.jpeg|" +
+                "png files (*.png)|*.png|" +
+                "tiff files (*.tiff; *.tif)|*.tiff; *.tif|" +
+                "All files (*.*)|*.*";
+        }
+
+        #endregion
+
+
+        #region PRIVATE
+
+        /// <summary>
+        /// Inverts image.
+        /// </summary>
+        /// <param name="inputImage">Image to invert.</param>
+        /// <param name="outputImage">Output image.</param>
+        private void InvertImage(Bitmap inputImage, out Bitmap outputImage)
+        {
+            // clone input image
+            outputImage = (Bitmap)inputImage.Clone();
+
+            try
+            {
+                // read bitmap to memory
+                BitmapData srcData = outputImage.LockBits(
+                    new Rectangle(0, 0, outputImage.Width, outputImage.Height),
+                    ImageLockMode.ReadWrite,
+                    outputImage.PixelFormat);
+
+                unsafe
+                {
+                    // get bytes per pixel
+                    int bytesPerPixel = Image.GetPixelFormatSize(outputImage.PixelFormat) / 8;
+                    // get image height
+                    int heightInPixels = srcData.Height;
+                    // get image width in bytes
+                    int widthInBytes = srcData.Width * bytesPerPixel;
+                    // get pointer to the first byte
+                    byte* ptrFirstPixel = (byte*)srcData.Scan0;
+
+                    // memory for pixel values
+                    byte oldBlue = 0;
+                    byte oldGreen = 0;
+                    byte oldRed = 0;
+
+                    // for each row
+                    for (int y = 0; y < heightInPixels; y++)
+                    {
+                        // get pointer to current row
+                        byte* currentLine = ptrFirstPixel + (y * srcData.Stride);
+                        // for each pixel in row
+                        for (int x = 0; x < widthInBytes; x += bytesPerPixel)
+                        {
+                            // get pixel value
+                            oldBlue = currentLine[x];
+                            oldGreen = currentLine[x + 1];
+                            oldRed = currentLine[x + 2];
+
+                            // calculate new pixel value
+                            currentLine[x] = (byte)(255 - oldBlue);
+                            currentLine[x + 1] = (byte)(255 - oldGreen);
+                            currentLine[x + 2] = (byte)(255 - oldRed);
+                        }
+                    }
+                }
+
+                outputImage.UnlockBits(srcData);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Opens an image when "OpenMenuItem" was clicked.
+        /// </summary>
+        /// <param name="sender">Caller object.</param>
+        /// <param name="e">Event information.</param>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // if user chose a file
+            if (_openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // if there is previous image
+                    if (_originalImage != null)
+                        // delete it
+                        _originalImage.Dispose();
+
+                    // if there is previous image
+                    if (_filteredImage!= null)
+                        // delete it
+                        _filteredImage.Dispose();
+
+                    // open new image
+                    _originalImage = new Bitmap(_openFileDialog.FileName);
+                    // set image to the viewer
+                    mainPictureBox.Image = _originalImage;
+                    originalPictureBox.Image = _originalImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Applyes Invert filter to the image.
+        /// </summary>
+        /// <param name="sender">Caller object.</param>
+        /// <param name="e">Event information.</param>
+        private void invertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InvertImage(_originalImage, out _filteredImage);
+
+            mainPictureBox.Image = _filteredImage;
+            filteredPictureBox.Image = _filteredImage;
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox sourceBox = sender as PictureBox;
+            if (sourceBox != mainPictureBox && sourceBox != null)
+                mainPictureBox.Image = sourceBox.Image;
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
